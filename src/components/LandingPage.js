0dd9f38e49d60490ceea3b5a4c8f14146eda7ab6
Alfonso <61892420+ShrikeFound@users.noreply.github.com>
2021-02-17 20:34:12 -0800

    added bot id to add button

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -7,7 +7,7 @@ const LandingPage = () => {
         <Container className="text-container">
           <h1>Challenge Your Destiny.</h1>
           <p>Add the Philethis Discord bot and other online tools and enjoy  playing Tabletop RPGs at a safe distance</p>
-          <a className="btn btn-primary btn-hero" href="https://discord.com/oauth2/authorize?client_id=INSERT_CLIENT_ID_HERE&scope=bot&permissions=658496" target="_blank" rel='noreferrer'>Add Lucius to Discord</a>
+          <a className="btn btn-primary btn-hero" href="https://discord.com/oauth2/authorize?client_id=800485041159012372&scope=bot&permissions=658496" target="_blank" rel='noreferrer'>Add Lucius to Discord</a>
         </Container>
       </Jumbotron>  
     </>
